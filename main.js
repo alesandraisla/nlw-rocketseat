636a6798fa35598013b2c5f12c404aee15dac854
AlesandraIsla <alesandramempis@gmail.com>
2021-10-22 14:55:27 -0300

    Ajustes no layout

--- main.js
+++ main.js
@@ -26,10 +26,10 @@ for(const link of links) {
 }
 
 // Mudar o header da página quando der scroll
+const header = document.querySelector('#header');
+const navHeight = header.offsetHeight;
 
 function changeHeaderWhenScroll() {
-  const header = document.querySelector('#header');
-  const navHeight = header.offsetHeight;
   if(window.scrollY >= navHeight) {
     //scroll é maior que a altura do header 
     header.classList.add('scroll')
@@ -75,8 +75,9 @@ footer .brand, footer .social
 `, { interval: 100})
 
 /**Botão voltar para o topo */
+const backToTopButton = document.querySelector('.back-to-top')
+
 function backToTop() {
-  const backToTopButton = document.querySelector('.back-to-top')
   
   if (window.scrollY >= 560) {
     backToTopButton.classList.add('show');
@@ -85,8 +86,33 @@ function backToTop() {
   }
 }
 
+/**Menu ativo conforme a seção visível na página */
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffSet + (window.innerHeight / 8 ) * 4
+  for(const section of sections) {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.offsetHeight;
+    const sectionId = section.getAttribute('id');
+    const checkpointStart = checkpoint >= sectionTop;
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight;
+
+    if(checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a [href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelectorAll('nav ul li a [href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+
+}
+
 /**When Scroll */
 window.addEventListener('scroll', () => {
   changeHeaderWhenScroll()
   backToTop()
+  activateMenuAtCurrentSection()
 })
